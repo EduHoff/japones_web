28c4a9900a707bee1b1bef381c6ecb07f9138511
Edu_Hoff <eduardohoffmanndocarmosilva@gmail.com>
2023-05-08 19:12:42 -0300

    inicio random
    modificações voltadas ao js para o funcionamento da etapa interativa do código.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,23 +1,79 @@
+let i = 0 //var global
+
+
+function randomNumberInterval(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1) + min)
+
+}
+
+
+
+function roleta(hira) {
+
+    if (i == 0) {
+        numAleatorio = randomNumberInterval(0, 0)
+    }
+
+    console.log('random ' + numAleatorio)
+
+    //parte do codigo responsavel por criar a dinamica do jogo
+    switch (numAleatorio) {
+        case 0:
+            alert('funciona')
+
+            hiraTela.textContent = 'あ'
+            hiraResp.textContent = ''
+            
+
+
+
+
+            ++i
+            console.log(i)
+
+
+
+
+            if (i == 3) {
+
+
+                if (hira == 'a') {
+                    hiraResp.textContent = 'acerto'
+                } else {
+                    hiraResp.textContent = 'errou'
+                }
+
+                
+                i = 0
+                hiraInput.value = ""
+            }
+
+
+            break;
+
+        default:
+            break;
+    }
+
+}
 
 const btn = document.querySelector('#hiraConfirm')
 
-btn.addEventListener("click", function(e){
+btn.addEventListener("click", function (e) {
 
     e.preventDefault();
 
-    const hira = document.querySelector("#hiraInput")
+    let hira = document.querySelector("#hiraInput").value
 
-    const hiraValor = hira.value
+    roleta(hira)
 
-    
 
-    console.log(hiraValor)
+    console.log(hira)
 
-    hiraTela.textContent = hiraValor
 
-    if(hiraValor == 'sim'){
-        alert('funciona!')
-    }
 
-    
+
+
+
 })
