7871084632dc98bf7c2b8f4fbc5ebadf3d8fd811
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-05-20 11:33:44 -0300

    website finalmente se tornou escalável! scoreboard funcionando, agora o site armazena além da pontuação o último charactere para evitar burlar o jogo, funcionalidade de focus no input e enter já funcionando e sem mais tantas linhas de código!

---
+++ Scripts/scoreboard.js
@@ -0,0 +1,16 @@
+export function scoreboard(point, mode) {
+
+    if (point === 1) {
+        const right = parseInt(localStorage.getItem(`right${mode}`) ?? "0") + 1;
+        localStorage.setItem(`right${mode}`, right.toString());
+    } else {
+        const wrong = parseInt(localStorage.getItem(`wrong${mode}`) ?? "0") + 1;
+        localStorage.setItem(`wrong${mode}`, wrong.toString());
+    }
+
+    const right = parseInt(localStorage.getItem(`right${mode}`) ?? "0");
+    const wrong = parseInt(localStorage.getItem(`wrong${mode}`) ?? "0");
+
+    const acc = right + wrong > 0 ? ((right / (right + wrong)) * 100).toFixed(2) : "0.00";
+    localStorage.setItem(`accuracy${mode}`, acc);
+}
